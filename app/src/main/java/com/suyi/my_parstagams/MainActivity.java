bb7dc632827acb683a5137e25b31f57e3179a281
schen019 <schen019@plattsburgh.edu>
2022-03-19 14:55:42 -0400

    update 0.3

--- app/src/main/java/com/suyi/my_parstagams/MainActivity.java
+++ app/src/main/java/com/suyi/my_parstagams/MainActivity.java
@@ -6,8 +6,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +13,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
