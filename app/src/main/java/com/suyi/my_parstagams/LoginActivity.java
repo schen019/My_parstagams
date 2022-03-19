bc1089e384865145fdd872741415b956e3470ee8
schen019 <schen019@plattsburgh.edu>
2022-03-19 00:01:35 -0400

    first updata

--- app/src/main/java/com/suyi/my_parstagams/LoginActivity.java
+++ app/src/main/java/com/suyi/my_parstagams/LoginActivity.java
@@ -1,11 +1,18 @@
 package com.suyi.my_parstagams;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +25,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
 
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
@@ -35,6 +46,24 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password) {
         Log.i(TAG,"Attempting to login user" + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    Log.e(TAG,"Login in failure", e);
+                    Toast.makeText(LoginActivity.this,"Oops! Login in Fail！", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success !",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
 
--- app/src/main/java/com/suyi/my_parstagams/ParseApplication.java
+++ app/src/main/java/com/suyi/my_parstagams/ParseApplication.java
@@ -2,10 +2,16 @@ package com.suyi.my_parstagams;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("oAtnYGH9248ARLn7YDZbwnbTCzU2MVOcgfjTqQqZ")
                 .clientKey("AmtEQzGnPPbTZ4AuZYjUogCFbjY1rSVeyyvDa2dG")
